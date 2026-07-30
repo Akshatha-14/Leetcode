51bdc8f860ba00c65b75b245ff387d083f3f1591
Akshatha-14 <kakshathashetty6@gmail.com>
2026-07-30 19:43:37 +0530

    Time: 17 ms (70.58%), Space: 12.6 MB (50.37%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def isAnagram(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        if len(s)!=len(t):
+            return False
+        dict={}
+        for ch in t:
+            if ch in dict:
+                dict[ch]+=1
+            else:
+                dict[ch]=1
+        for ch in s:
+            if ch not in dict or dict[ch]==0:
+                return False
+            else:
+                dict[ch]-=1
+        return True
